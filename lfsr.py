3505e8691e26810d87265acdfaa20b4f5f506338
Florian Best <best@univention.de>
2019-01-24 16:52:30 +0100

    Fix bm(string)
    The name "xor" was not defined.
    map(int, tuple('string')) is wrong and causes a crash.

--- lfsr.py
+++ lfsr.py
@@ -15,7 +15,9 @@
 # History:      None yet
 # TODO:         Nothing yet
 #------------------------------------------------------------------------
-class GLFSR:
+
+
+class GLFSR(object):
     def __init__(self, polynomial, seed):
         self.polynomial = polynomial | 1
         self.seed = seed
@@ -26,7 +28,8 @@ class GLFSR:
         while temp_mask != 0:
             if temp_mask & self.mask != 0:
                 temp_mask = temp_mask ^ self.mask
-            if temp_mask == 0: break
+            if temp_mask == 0:
+                break
             self.mask = self.mask << 1
 
     def states(self, repeat=False):
@@ -37,14 +40,15 @@ class GLFSR:
                 yield 1
             else:
                 yield 0
-            if repeat == False and self.data == self.seed:
+            if not repeat and self.data == self.seed:
                 return
 
 
 # Polynomials needed below this point
 from polynomial import Polynomial
-X   = Polynomial([1, 0])
+X = Polynomial([1, 0])
 One = Polynomial([1])
+
 
 def bm(seq):
     """Implementation of the Berlekamp-Massey algorithm, the purpose
@@ -69,18 +73,18 @@ def bm(seq):
        LFSR. Here we have f(x) = c0 * x^m + c1 * x^{m-1} +...+ c{m-1} * x + 1.
        """
     # Allow for an input string along with a list or tuple
-    if type(seq) == type('string'):
-        seq = map(int, tuple(seq))
+    if isinstance(seq, str):
+        seq = map(ord, tuple(seq))
 
     m = 0
 
     # N is the index of the current element of the sequence SEQ under consideration
     # f is the characteristic polynomial of the current LFSR
-    N,f = 0,One
+    N, f = 0, One
 
     # N0 and f0 are the values of N and f when m was last changed
     # N0 starts out as -1
-    N0,f0 = -1,One
+    N0, f0 = -1, One
 
     n = len(seq)
     while N < n:
@@ -92,14 +96,14 @@ def bm(seq):
             # the 'X' occuring below is a global variable and
             # is the indeterminant in the polynomials defined
             # by the class 'Polynomial'. That is, X=Polynomial([1,0])
-            if 2*m>N:
-                f = f + f0 * X**(N-N0)
+            if 2 * m > N:
+                f = f + f0 * X**(N - N0)
 
             # Otherwise we'll have to update everything
             else:
-                f0, f = f, f + f0 * X**(N-N0)
+                f0, f = f, f + f0 * X**(N - N0)
                 N0 = N
-                m = N+1-m
+                m = N + 1 - m
 
         # Next element..
         N += 1
--- polynomial.py
+++ polynomial.py
@@ -11,18 +11,20 @@
 # History:     None yet
 # TODO:        None yet
 #------------------------------------------------------------------------
+import operator
+
 
 class Polynomial(list):
     def __str__(self):
-        L = ['{}*x^{}'.format(e, i) for i,e in enumerate(self)]
+        L = ['{}*x^{}'.format(e, i) for i, e in enumerate(self)]
         return ' + '.join(L)
 
     def __add__(self, other):
         if len(self) < len(other):
-              return other + self
+            return other + self
         else:
             k = len(self) - len(other)
-            new = [a^b for (a,b) in zip(self,([0]*k)+other)]
+            new = [a ^ b for (a, b) in zip(self, ([0] * k) + other)]
             try:
                 while new[0] == 0:
                     new.pop(0)
@@ -30,31 +32,31 @@ class Polynomial(list):
                 new = []
             return Polynomial(new)
 
-    def dot(self,S):
+    def dot(self, S):
         """ This returns the sum of ck*S{N-k} where N+1 = len(S),
             and k runs from 0 to m-1 (m is my degree)
         """
         m = len(self)
         N = len(S)
         C = self[:-1]
-        S = S[N-m+1:]
-        return dot(C,S)
+        S = S[N - m + 1:]
+        return dot(C, S)
 
-    def __mul__(self,other):
+    def __mul__(self, other):
         deg = len(self) + len(other) - 2
-        prod = [0]*(deg + 1)
-        for k,x in enumerate(self):
-            for j,y in enumerate(other):
-                prod[k+j] ^= (x&y)
+        prod = [0] * (deg + 1)
+        for k, x in enumerate(self):
+            for j, y in enumerate(other):
+                prod[k + j] ^= (x & y)
         return Polynomial(prod)
 
-    def __pow__(self,ex):
-        if ex==0:
+    def __pow__(self, ex):
+        if ex == 0:
             return Polynomial([1])
         else:
-            return self*(self**(ex-1))
+            return self * (self**(ex - 1))
 
 
 def dot(x, y):
-        """Returns the dot product of two lists."""
-        return reduce(xor, [a&b for (a,b) in zip(x,y)], 0)
+    """Returns the dot product of two lists."""
+    return reduce(operator.xor, [a & b for (a, b) in zip(x, y)], 0)
